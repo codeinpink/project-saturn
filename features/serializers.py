d827e912439f1335758db1b360237e584a5d5ddf
Shannon <shannon.babincsak@gmail.com>
2015-09-10 14:38:44 -0500

    Added depth and related fields to some of the REST model serializers (Team, Feature, Commitment)

--- features/serializers.py
+++ features/serializers.py
@@ -1,22 +1,36 @@
 from .models import Team, Feature, Commitment, Risk, Dependency
 from rest_framework import serializers
 
-class TeamSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Team
-
-class FeatureSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Feature
-
-class CommitmentSerializer(serializers.HyperlinkedModelSerializer):
+class CommitmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Commitment
+        fields = ('id', 'name', 'team', 'feature', 'done_definition', 'status',
+                'start', 'stop', 'notes', 'risk_set', 'dependency_set')
+        depth = 1
 
-class RiskSerializer(serializers.HyperlinkedModelSerializer):
+class FeatureSerializer(serializers.ModelSerializer):
+    commitment_set = CommitmentSerializer(many=True)
+
+    class Meta:
+        model = Feature
+        fields = ('id', 'name', 'description', 'theme', 'teams', 'clarity_or_jira_id',
+                'commitment_set')
+        depth = 2
+
+class TeamSerializer(serializers.ModelSerializer):
+    commitment_set = CommitmentSerializer(many=True)
+
+    class Meta:
+        model = Team
+        fields = ('id', 'name', 'bug_buffer', 'num_jiras', 'confidence',
+                'previous_unplanned_work', 'planned_unplanned_work', 'slug', 'feature_set',
+                'commitment_set')
+        depth = 2
+
+class RiskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Risk
-        
-class DependencySerializer(serializers.HyperlinkedModelSerializer):
+
+class DependencySerializer(serializers.ModelSerializer):
     class Meta:
         model = Dependency
--- saturn/settings.py
+++ saturn/settings.py
@@ -103,7 +103,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
