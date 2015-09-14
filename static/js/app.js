97a62147e4ac62114775198a8ef34a152878cd7a
Shannon <shannon.babincsak@gmail.com>
2015-09-14 16:41:29 -0500

    Added angular data tables and ngResource

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-var saturnApp = angular.module('missionControlApp',['ui.bootstrap', 'angucomplete-alt']);
+var saturnApp = angular.module('missionControlApp',['ui.bootstrap', 'ngResource', 'angucomplete-alt', 'datatables']);
 
 saturnApp.config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[{');
--- static/js/controllers/teamViewCtrl.js
+++ static/js/controllers/teamViewCtrl.js
@@ -1,16 +1,43 @@
 var TEAM_COLUMN_LABELS = ['Feature','Commitment', 'Commit Status','Def. of Done','Start Iteration','Finish Iteration','Comments'];
 
-
-
-
-
-saturnApp.controller("teamViewCtrl",['$scope','$http',
-	function($scope,$http){
+saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', 'DTOptionsBuilder', 'DTColumnDefBuilder',
+	function($scope, $http, $resource, DTOptionsBuilder, DTColumnDefBuilder){
 		$scope.teamId = $('#teamId').html();
 		$scope.columns=TEAM_COLUMN_LABELS;
-		$scope.teamObj = {}
 
-		refreshCommits($scope,$http);
+		/* TODO: Move resource into factory?
+		see:	https://docs.angularjs.org/api/ngResource/service/$resource
+				http://www.sitepoint.com/creating-crud-app-minutes-angulars-resource/
+		*/
+		var Team = $resource('/api/teams/:id/')
+		$scope.teamObj = Team.get({id: $scope.teamId}), function() {
+			console.log($scope.teamObj);
+		};
+
+		//refreshCommits($scope,$http);
+
+		$scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers');
+
+    	$scope.dtColumnDefs = [
+			DTColumnDefBuilder.newColumnDef(0),
+			DTColumnDefBuilder.newColumnDef(1),
+			DTColumnDefBuilder.newColumnDef(2),
+			DTColumnDefBuilder.newColumnDef(3),
+			DTColumnDefBuilder.newColumnDef(4),
+			DTColumnDefBuilder.newColumnDef(5),
+			DTColumnDefBuilder.newColumnDef(6),
+			DTColumnDefBuilder.newColumnDef(7).notSortable(),
+			DTColumnDefBuilder.newColumnDef(8).notSortable(),
+		];
+
+		$scope.editCommitment = function(index) {
+			console.log(index);
+		};
+
+		$scope.deleteCommitment = function(index) {
+			console.log(index);
+		};
+
 }]);
 
 function refreshCommits($scope,$http){
