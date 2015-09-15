5ef0f89c0296b237d8db9cd2af106ca56ea6f460
Shannon <shannon.babincsak@gmail.com>
2015-09-15 14:05:12 -0500

    Added the ability to edit and delete commitments on the team page

--- static/js/controllers/teamViewCtrl.js
+++ static/js/controllers/teamViewCtrl.js
@@ -1,15 +1,13 @@
 var TEAM_COLUMN_LABELS = ['Feature','Commitment', 'Commit Status','Def. of Done','Start Iteration','Finish Iteration','Comments'];
 
-saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'Team',
-	function($scope, $http, $resource, DTOptionsBuilder, DTColumnDefBuilder, Team){
+saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', '$modal', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'Team', 'Commitment',
+	function($scope, $http, $resource, $modal, DTOptionsBuilder, DTColumnDefBuilder, Team, Commitment){
 		$scope.teamId = $('#teamId').html();
 		$scope.columns=TEAM_COLUMN_LABELS;
 
 		$scope.teamObj = Team.get({id: $scope.teamId}), function() {
 			console.log($scope.teamObj);
 		};
-
-		//refreshCommits($scope,$http);
 
 		$scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers');
 
@@ -25,15 +23,72 @@ saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', 'DTOptionsBu
 			DTColumnDefBuilder.newColumnDef(8).notSortable(),
 		];
 
-		$scope.editCommitment = function(index) {
-			console.log(index);
+		$scope.editCommitment = function(size, commitment) {
+			var modalInstance = $modal.open({
+				animation: true,
+				templateUrl: '/static/js/templates/editCommitment.html',
+				controller: 'EditCommitmentCtrl',
+				size: size,
+				resolve: {
+		        	commitment: function () {
+						return commitment;
+		        	}
+				}
+		    });
 		};
 
-		$scope.deleteCommitment = function(index) {
-			console.log(index);
+		$scope.showConfirmationPrompt = function(size, id) {
+			var modalInstance = $modal.open({
+				animation: true,
+				templateUrl: '/static/js/templates/deleteConfirmation.html',
+				controller: 'DeleteCommitmentCtrl',
+				size: size,
+				resolve: {
+					id: function () {
+						return id;
+					}
+				}
+		    });
 		};
 
 }]);
+
+
+saturnApp.controller('DeleteCommitmentCtrl', function($scope, $modalInstance, id, Commitment) {
+	$scope.commitment_id = id;
+
+	$scope.deleteCommitment = function() {
+		Commitment.delete({id: $scope.commitment_id}, function() {
+			console.log("Commitment deleted.");
+		});
+
+		$modalInstance.close();
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+});
+
+saturnApp.controller('EditCommitmentCtrl', function($scope, $modalInstance, commitment, Commitment) {
+	$scope.commitment = commitment;
+	$scope.iterationOptions = PSI_CYCLES;
+	$scope.commitmentStatusOptions = COMMITMENT_STATUS_OPTIONS;
+	$scope.defOfDoneOptions = DEFINITION_OF_DONE_OPTIONS;
+
+	$scope.updateCommitment = function() {
+		Commitment.update({id: $scope.commitment.id}, $scope.commitment, function() {
+			console.log("Updated");
+		});
+
+	  $modalInstance.close();
+	};
+
+	$scope.cancel = function () {
+    	$modalInstance.dismiss('cancel');
+	};
+});
+
 
 function refreshCommits($scope,$http){
 	$http.get('/api/teams/'+$scope.teamId).then(function(response) {
--- static/js/services/services.js
+++ static/js/services/services.js
@@ -1,3 +1,11 @@
 saturnApp.factory('Team', function($resource) {
-    return $resource('/api/teams/:id');
+    return $resource('/api/teams/:id/');
+});
+
+saturnApp.factory('Commitment', function($resource) {
+    return $resource('/api/commitments/:id/', {id: '@id'}, {
+        update: {
+            method: 'PUT'
+        }
+    });
 });
