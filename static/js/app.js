303c2538ee43f790210349653423af2edf52647d
Shannon <shannon.babincsak@gmail.com>
2015-09-15 14:01:15 -0500

    Added 'constants' to hold options for various fields and changed  to keep trailing slashes

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,30 @@ saturnApp.config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[{');
   $interpolateProvider.endSymbol('}]}');
 });
+
+saturnApp.config(function($resourceProvider) {
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+});
+
+var PSI_CYCLES = [
+    '18-1',
+    '18-2',
+    '18-3',
+    '18-4',
+    '18-5'
+];
+
+var COMMITMENT_STATUS_OPTIONS = [
+    'COMMIT',
+    'STRETCH'
+];
+
+var DEFINITION_OF_DONE_OPTIONS = [
+    'READY FOR SYSTEM/PQ TEST',
+    'MOVE TO STATUS 22',
+    'MOVE TO STATUS 25',
+    'DESIGN SPIKE COMPLETE',
+    'PASSING DATA',
+    'PACKAGES RELEASED',
+    'OTHER - DESCRIBED IN COMMENTS'
+]
