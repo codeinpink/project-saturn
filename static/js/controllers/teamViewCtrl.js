c54b57fa005c31a6c6a5d8ee063d603ff6176e7d
Shannon <shannon.babincsak@gmail.com>
2015-09-15 16:51:59 -0500

    Added modal to show risks table

--- static/js/controllers/teamViewCtrl.js
+++ static/js/controllers/teamViewCtrl.js
@@ -1,9 +1,9 @@
 var TEAM_COLUMN_LABELS = ['Feature','Commitment', 'Commit Status','Def. of Done','Start Iteration','Finish Iteration','Comments'];
 
 saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', '$modal', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'Team', 'Commitment',
-	function($scope, $http, $resource, $modal, DTOptionsBuilder, DTColumnDefBuilder, Team, Commitment){
+	function($scope, $http, $resource, $modal, DTOptionsBuilder, DTColumnDefBuilder, Team, Commitment) {
 		$scope.teamId = $('#teamId').html();
-		$scope.columns=TEAM_COLUMN_LABELS;
+		$scope.columns = TEAM_COLUMN_LABELS;
 
 		$scope.teamObj = Team.get({id: $scope.teamId}), function() {
 			console.log($scope.teamObj);
@@ -21,7 +21,37 @@ saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', '$modal', 'D
 			DTColumnDefBuilder.newColumnDef(6),
 			DTColumnDefBuilder.newColumnDef(7).notSortable(),
 			DTColumnDefBuilder.newColumnDef(8).notSortable(),
+			DTColumnDefBuilder.newColumnDef(9).notSortable(),
+			DTColumnDefBuilder.newColumnDef(10).notSortable(),
 		];
+
+		$scope.showRisks = function(commitment) {
+			var modalInstance = $modal.open({
+				animation: true,
+				templateUrl: '/static/js/templates/showRisks.html',
+				controller: 'RiskCtrl',
+				size: 'lg',
+				resolve: {
+		        	commitment: function () {
+						return commitment;
+		        	}
+				}
+		    });
+		};
+
+		$scope.showDependencies = function(commitment) {
+			var modalInstance = $modal.open({
+				animation: true,
+				templateUrl: '/static/js/templates/editCommitment.html',
+				controller: 'DependencyCtrl',
+				size: 'lg',
+				resolve: {
+		        	commitment: function () {
+						return commitment;
+		        	}
+				}
+		    });
+		}
 
 		$scope.editCommitment = function(size, commitment) {
 			var modalInstance = $modal.open({
@@ -53,6 +83,43 @@ saturnApp.controller("teamViewCtrl",['$scope','$http', '$resource', '$modal', 'D
 
 }]);
 
+saturnApp.controller('RiskCtrl', function($scope, $modalInstance, commitment, Commitment) {
+	$scope.commitment = commitment;
+
+	$scope.generalOptions = RISK_GENERAL_OPTIONS;
+	$scope.resolutionOptions = RISK_RESOLUTION_OPTIONS;
+
+	$scope.updateCommitment = function() {
+		Commitment.update({id: $scope.commitment.id}, $scope.commitment, function() {
+			console.log("Updated");
+		});
+
+	  $modalInstance.close();
+	};
+
+	$scope.close = function () {
+    	$modalInstance.dismiss('cancel');
+	};
+});
+
+saturnApp.controller('DependencyCtrl', function($scope, $modalInstance, commitment, Commitment) {
+	$scope.commitment = commitment;
+	$scope.iterationOptions = PSI_CYCLES;
+	$scope.commitmentStatusOptions = COMMITMENT_STATUS_OPTIONS;
+	$scope.defOfDoneOptions = DEFINITION_OF_DONE_OPTIONS;
+
+	$scope.updateCommitment = function() {
+		Commitment.update({id: $scope.commitment.id}, $scope.commitment, function() {
+			console.log("Updated");
+		});
+
+	  $modalInstance.close();
+	};
+
+	$scope.cancel = function () {
+    	$modalInstance.dismiss('cancel');
+	};
+});
 
 saturnApp.controller('DeleteCommitmentCtrl', function($scope, $modalInstance, id, Commitment) {
 	$scope.commitment_id = id;
