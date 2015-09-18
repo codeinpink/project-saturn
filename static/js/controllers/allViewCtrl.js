7fd722e672328d21c8a62ac69381988a0c61c946
Rain Li <yrl0001@auburn.edu>
2015-09-17 20:08:40 -0500

    working on risk/dependency tabs on all page. also chanegs to the base/ added button for home.

--- static/js/controllers/allViewCtrl.js
+++ static/js/controllers/allViewCtrl.js
@@ -1,21 +1,20 @@
-var ALL_FEATURE_COLUMN_LABELS = ['Feature','Commitment','Team','Commit Status','Def. of Done','Start Iteration','Finish Iteration','Comments'];
+var ALL_FEATURE_COLUMN_LABELS = ['Feature','Theme','Commitment','Team','Commit Status','Def. of Done','Start Iteration','Finish Iteration','Comments'];
 
-saturnApp.controller("allViewCtrl",['$scope','$http', '$resource', '$modal', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'Commitments',
-	function($scope, $http, $resource, $modal, DTOptionsBuilder, DTColumnDefBuilder, Commitments){
+saturnApp.controller("allViewCtrl",['$scope','$http', '$resource', '$modal', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'Commitments','Risks','Dependencies',
+	function($scope, $http, $resource, $modal, DTOptionsBuilder, DTColumnDefBuilder, Commitments,Risks,Dependencies){
 		$scope.columns=ALL_FEATURE_COLUMN_LABELS;
 		$scope.commitments =null;
+        $scope.risks =null;
+        $scope.dependencies =null;
 		$scope.reloadCommits = function(){refreshCommits();};
-		function refreshCommits() {
-			Commitments.query(function (commits) {
-                $scope.commitments = commits;
-                
-            });
-		}
+		$scope.reloadRisk = function(){refreshRisk();};
+        $scope.reloadDependencies = function(){refreshDependencies();};
         refreshCommits();
+        refreshRisks();
+        refreshDependencies();
 
 		$scope.dtOptions = DTOptionsBuilder.newOptions().withTableTools('/static/lib/copy_csv_xls_pdf.swf')
         .withTableToolsButtons([
-            'copy',
             'print', {
                 'sExtends': 'collection',
                 'sButtonText': 'Save',
@@ -30,7 +29,23 @@ saturnApp.controller("allViewCtrl",['$scope','$http', '$resource', '$modal', 'DT
 			DTColumnDefBuilder.newColumnDef(4),
 			DTColumnDefBuilder.newColumnDef(5),
 			DTColumnDefBuilder.newColumnDef(6),
+            DTColumnDefBuilder.newColumnDef(7),
 		];
+        function refreshCommits() {
+            Commitments.query(function (commits) {
+                $scope.commitments = commits;
+            });
+        }
+        function refreshRisks() {
+            Risks.query(function (risks) {
+                $scope.risks = risks;
+            });
+        }
+        function refreshDependencies() {
+            Dependencies.query(function (dependencies) {
+                $scope.dependencies = dependencies;
+            });
+        }
 
 
 }]);
