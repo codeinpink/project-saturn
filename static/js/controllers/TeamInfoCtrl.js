845cb5932481c2ecab2e3780bf96f0fd15a99bdc
Shannon <shannon.babincsak@gmail.com>
2015-09-18 22:10:18 -0500

    Changed PSI Capacity validation to be consistent with other forms

--- static/js/controllers/TeamInfoCtrl.js
+++ static/js/controllers/TeamInfoCtrl.js
@@ -1,11 +1,16 @@
 saturnApp.controller('TeamInfoCtrl', function($scope, $modalInstance, Team, team) {
 	$scope.team = angular.copy(team);
+	$scope.submitted = false;
 
     $scope.saveTeamInfo = function() {
-		Team.update({id: $scope.team.id}, $scope.team, function(team) {
-			console.log("Updated");
-			$modalInstance.close(team);
-		});
+    	$scope.submitted = true;
+
+    	if ($scope.team.confidence > 0) {
+    		Team.update({id: $scope.team.id}, $scope.team, function(team) {
+				console.log("Updated");
+				$modalInstance.close(team);
+			});
+    	}
 	};
 
 	$scope.cancel = function () {
