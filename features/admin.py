aeb00cb254826b5bdc011b09d2dcd2b3b53992d3
Shannon <shannon.babincsak@gmail.com>
2015-09-24 10:54:58 -0500

    Team list on admin page now shows confidence next to team, and teams can now be filtered by confidence (simple filter)

--- features/admin.py
+++ features/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Team, Feature, Commitment, Risk, Dependency
 
-admin.site.register([Team, Feature, Commitment, Risk, Dependency])
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'confidence',)
+    list_filter = ('confidence',)
+
+admin.site.register([Feature, Commitment, Risk, Dependency])
