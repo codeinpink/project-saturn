d2ef4cd3aca6fbec71defa8300cb80aaf06b85e7
Shannon Babincsak <shannon.babincsak@gmail.com>
2015-10-05 16:16:37 -0500

    Added tests for Team REST

--- features/tests.py
+++ features/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.core.urlresolvers import reverse
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
-from .models import Team
+from .models import Team, Feature, Commitment, Risk, Dependency
+from .serializers import TeamSerializer, FeatureSerializer, CommitmentSerializer, \
+                        RiskSerializer, DependencySerializer
 
 class TeamTestCase(TestCase):
     def setUp(self):
@@ -22,3 +27,43 @@ class TeamTestCase(TestCase):
         team.name = '4th Rail'
         team.save()
         self.assertEqual(team.slug, '4th-rail')
+
+class TeamRestTestCase(APITestCase):
+    def setUp(self):
+        Team.objects.create(name='3rd Rail')
+
+    def test_query(self):
+        '''Teams can be queried and result contains necessary data'''
+
+        team = Team.objects.get()
+
+        response = self.client.get(reverse('team-detail', args=[team.id]), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, TeamSerializer(team).data)
+
+        response = self.client.get(reverse('team-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, [TeamSerializer(team).data])
+
+        team2 = Team.objects.create(name='4th Rail')
+
+        response = self.client.get(reverse('team-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, [TeamSerializer(team).data, TeamSerializer(team2).data])
+
+    def test_update(self):
+        '''Team data can be updated'''
+
+        team = TeamSerializer(Team.objects.get()).data
+        team['confidence'] = 3.4
+        team['planned_unplanned_work'] = 10.2
+        team['previous_unplanned_work'] = 33.3
+
+        response = self.client.put(reverse('team-detail', args=[team['id']]), team, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, TeamSerializer(team).data)
+
+        team = Team.objects.get()
+        self.assertEqual(team.confidence, 3.4)
+        self.assertEqual(team.planned_unplanned_work, 10.2)
+        self.assertEqual(team.previous_unplanned_work, 33.3)
